a220275fd585c4147d30707facedb51ff12ede4e
acamp <acampbell8610@gmail.com>
2022-05-16 20:43:35 -0500

    cart ui

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -4,12 +4,16 @@ import { useProductReducer } from './reducers';
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
+
+
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: ''
+      });
     // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
